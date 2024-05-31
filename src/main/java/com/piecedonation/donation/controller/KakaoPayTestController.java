1db422218d618ad06bf96e1511c4f7bb6dbe3e7e
hum02 <ehdgma02@gmail.com>
2024-05-31 16:36:28 +0900

    test:스마트 컨트랙 테스트용 토큰전송 api 추가

--- src/main/java/com/piecedonation/donation/controller/KakaoPayTestController.java
+++ src/main/java/com/piecedonation/donation/controller/KakaoPayTestController.java
@@ -1,18 +1,41 @@
 package com.piecedonation.donation.controller;
 
+import com.piecedonation.donation.domain.MemberRepository;
+import com.piecedonation.donation.service.blockchain.LuniverseClient;
+import com.piecedonation.donation.service.blockchain.WalletService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KakaoPayTestController {
 
+    LuniverseClient luniverseClient;
+
+    WalletService walletService;
+
+    MemberRepository memberRepository;
+
+    public KakaoPayTestController(LuniverseClient luniverseClient, WalletService walletService, MemberRepository memberRepository) {
+        this.luniverseClient = luniverseClient;
+        this.walletService = walletService;
+        this.memberRepository = memberRepository;
+    }
+
     @GetMapping("/success")
-    public String success(){
+    public String success() {
         return "success";
     }
 
+    @PostMapping("/transfer")
+    public String tranfer(@RequestParam("to") String to) {
+        luniverseClient.transferTokenToMemberWallet(to);
+        return "transfer success";
+    }
+
     @GetMapping("/fail")
-    public String fail(){
+    public String fail() {
         return "fail";
     }
 }
