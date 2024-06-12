1645983ac51e8a20e2efba3668ee23da6146576a
hum02 <ehdgma02@gmail.com>
2024-06-12 11:15:30 +0900

    fix: 오타 수정

--- src/main/java/com/piecedonation/donation/domain/MemberRepository.java
+++ src/main/java/com/piecedonation/donation/domain/MemberRepository.java
@@ -2,7 +2,7 @@ package com.piecedonation.donation.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-행import java.util.Optional;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findById(String memberId);
